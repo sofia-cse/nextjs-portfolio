6b2b6f2df3e12e6ddab35289bb914e9b89619a3c
Sofia Martin <sofiamartinux@gmail.com>
2023-10-04 19:14:21 -0700

    adjusted top margin on main logo

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
   return (
     <div id="main" className="w-full h-[75vh] text-center">
       <div className="max-w-[800px] relative w-full h-full mx-auto p-2 flex justify-center items-start">
-        <div className="flex-col items-center mt-32">
+        <div className="flex-col items-center mt-20">
           <div className="center">
             <Image
               src={logo}
